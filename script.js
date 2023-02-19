ad839ac1fe123e7314aca803bcaa185e0acc4b07
Deny Anwar <denyanwardev@gmail.com>
2023-02-19 09:56:46 +0800

    finished all function app

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const weather = document.querySelector('.weather');
-const searchButton = document.querySelector('button[role="search"]');
+const searchBtn = document.querySelector('button[role="search"]');
 const cityField = document.querySelector('input[type="search"]');
 const forecastRow = document.querySelector('.forecast > .row');
 const dateElement = document.querySelector('.header__date');
-const geolocateIcones = document.querySelector('.header__geolocate');
+const geolocateIcons = document.querySelectorAll('.header__geolocate');
 
 const icons = {
 	'01d': 'wi-day-sunny',
@@ -34,5 +34,103 @@ function printTodayDate() {
 	dateElement.insertAdjacentText('afterbegin', today.toLocaleString('en-us', options));
 }
 
+function getWeekDay(date) {
+  const options = { weekday: 'long' };
+  return date.toLocaleString('en-us', options);
+}
+
+function removeChildren(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function renderForecast(forecast) {
+  removeChildren(forecastRow);
+  forecast.forEach((weatherData) => {
+    const markup = `<div class="forecast__day">
+      <h3 class="forecast__date">${getWeekDay(new Date(weatherData.dt * 1000))}</h3>
+      <i class='wi ${icons[weatherData.weather[0].icon]} forecast__icon'></i>
+      <p class="forecast__temp">${Math.floor(weatherData.main.temp)}°C</p>
+      <p class="forecast__desc">${weatherData.weather[0].main}</p>
+    </div>`;
+    forecastRow.insertAdjacentHTML('beforeend', markup);
+  });
+}
+
+function getForecast(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const forecastData = data.list.filter((obj) => obj.dt_txt.endsWith('06:00:00'));
+      renderForecast(forecastData);
+    })
+    .catch(() => {
+      removeChildren(forecastRow);
+    });
+}
+
+function getCityWeather(url) {
+  fetch(url) 
+    .then((response) => response.json())
+    .then((data) => {
+      const markup = `<h1 class="location">${data.name}, ${data.sys.country}</h1>
+      <div class="weather__summary">
+        <p><i class="wi ${icons[data.weather[0].icon]} weather-icon"></i> <span class="weather__celcius-value">${Math.floor(data.main.temp)}°C</span></p>
+        <p>${data.weather[0].main}</p>
+        <ul class="weather__miscellaneous">
+          <li><i class="wi wi-humidity"></i> Humidity <span>${data.main.humidity}%</span></li>
+          <li><i class="wi wi-small-craft-advisory"></i> Wind Speed <span>${data.wind.speed} m/s</span></li>
+        </ul>
+      </div>`;
+      removeChildren(weather);
+      weather.insertAdjacentHTML('beforeend', markup);
+    })
+    .catch((error) => {
+      const markup = `<h1 class="location">Error: city not found</h1>
+      <div class="weather__summary">
+      <p><i class="wi ${icons['09n']} forecast__icon"></i></p>
+      </div>`;
+      removeChildren(weather);
+      weather.insertAdjacentHTML('beforeend', markup);
+    })
+}
+
+function getWeatherByCoordinates(latitude, longitude) {
+  getCityWeather(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=b77340146fae68893e1f56b08d3190e9&units=metric`);
+}
+
+function getForecastByCoordinates(latitude, longitude) {
+  getForecast(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&APPID=b77340146fae68893e1f56b08d3190e9&units=metric`);
+}
+
+function getWeatherByCity(city) {
+  getCityWeather(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=b77340146fae68893e1f56b08d3190e9&units=metric`);
+}
+
+function getForecastByCity(city) {
+  getForecast(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=b77340146fae68893e1f56b08d3190e9&units=metric`);
+}
+
+function geoSuccess(position) {
+  const { latitude, longitude } = position.coords;
+  getWeatherByCoordinates(latitude, longitude);
+  getForecastByCoordinates(latitude, longitude);
+}
 
 printTodayDate();
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  getWeatherByCity(cityField.value);
+  getForecastByCity(cityField.value);
+});
+
+geolocateIcons.forEach((icon) => {
+  icon.addEventListener('click', (e) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(geoSuccess);
+    } else {
+      alert('Your browser does not suppor geolocation');
+    }
+  });
+});
